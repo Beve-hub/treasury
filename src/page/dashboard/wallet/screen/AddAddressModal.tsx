71d3c2b3c53e7d0a63beb2e9d15d4b3fedb824ee
Believe <victorokeke89@gmail.com>
2024-05-28 02:12:58 +0000

    props

--- src/page/dashboard/wallet/screen/AddAddressModal.tsx
+++ src/page/dashboard/wallet/screen/AddAddressModal.tsx
@@ -1,20 +1,17 @@
-interface Props {
+interface AddAddressModalProps {
   showAddAddressModal: boolean;
   onClose: () => void;
   children: React.ReactNode;
 }
 
-const AddAddressModal = ({ showAddAddressModal,  onClose, children }:Props) => {
-  
+const AddAddressModal: React.FC<AddAddressModalProps> = ({ showAddAddressModal, onClose, children }) => {
   return (
     <div onClick={onClose} className={`fixed inset-0 flex justify-center items-center ${showAddAddressModal ? "visible bg-black/20" : "invisible"}`}>
-      <div className={`bg-white p-4 rounded-md ${showAddAddressModal ? 'scale-100 opacity-100' : 'scale-110 opacity-0'}`} 
-      onClick={(e) => e.stopPropagation()}
-      >
+      <div className={`bg-white p-4 rounded-md ${showAddAddressModal ? 'scale-100 opacity-100' : 'scale-110 opacity-0'}`} onClick={(e) => e.stopPropagation()}>
         <button onClick={onClose} className="absolute top-2 right-2 bg-gray-200 rounded-full w-6 h-6 flex justify-center items-center">
           X
         </button>
-       {children}
+        {children}
       </div>
     </div>
   );
--- src/page/dashboard/wallet/screen/AddCardModal.tsx
+++ src/page/dashboard/wallet/screen/AddCardModal.tsx
@@ -1,23 +1,20 @@
-interface Props {
-    showAddCardModal: boolean;
-    onClose: () => void;
-    children: React.ReactNode;
+interface AddCardModalProps {
+  showAddCardModal: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
 }
 
-const AddCardModal = ({ showAddCardModal,  onClose, children }:Props) => {
-   
-    return (
-        <div onClick={onClose} className={`fixed inset-0 flex justify-center items-center ${showAddCardModal ? "visible bg-black/20" : "invisible"}`}>
-      <div className={`bg-white p-4 rounded-md ${showAddCardModal ? 'scale-100 opacity-100' : 'scale-110 opacity-0'}`} 
-      onClick={(e) => e.stopPropagation()}
-      >
+const AddCardModal: React.FC<AddCardModalProps> = ({ showAddCardModal, onClose, children }) => {
+  return (
+    <div onClick={onClose} className={`fixed inset-0 flex justify-center items-center ${showAddCardModal ? "visible bg-black/20" : "invisible"}`}>
+      <div className={`bg-white p-4 rounded-md ${showAddCardModal ? 'scale-100 opacity-100' : 'scale-110 opacity-0'}`} onClick={(e) => e.stopPropagation()}>
         <button onClick={onClose} className="absolute top-2 right-2 bg-gray-200 rounded-full w-6 h-6 flex justify-center items-center">
           X
         </button>
-       {children}
+        {children}
       </div>
     </div>
-    )
-}
+  );
+};
 
-export default AddCardModal
+export default AddCardModal;
